54a3f4915e0668ba931598f657eb38b2d619c8bd
DavidSchuchert <Davidschuchert@icloud.com>
2023-09-08 10:33:00 +0200

    added Image to char and Chicken

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -3,8 +3,10 @@ class MovableObject {
   y = 400;
   img;
 
+  //loadImage('./img/img.png')
   loadImage(path) {
     this.img = new Image();
+    this.img.src = path;
   }
   moveRight() {
     console.log("Moving right");
