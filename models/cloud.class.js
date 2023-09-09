58294b6c6c1146f984a5322ad18fdcafb727c8a4
DavidSchuchert <Davidschuchert@icloud.com>
2023-09-09 11:58:06 +0200

    added animation to character and clouds

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -9,5 +9,14 @@ class Cloud extends MovableObject{
     
         this.x = Math.random() * 500;
 
+
+      this.animate();
+
+
+      }
+      animate() {
+        setInterval(() => {
+          this.x -= 0.15;
+        }, 1000 / 60);
       }
 }
