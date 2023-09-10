9c0eee48f5161a79868f74250e22071a7c89e5a7
DavidSchuchert <Davidschuchert@icloud.com>
2023-09-10 15:23:28 +0200

    made character walk left picture change

--- models/character.class.js
+++ models/character.class.js
@@ -1,52 +1,47 @@
 class Character extends MovableObject {
+  height = 280;
+  y = 155;
+  speed = 10;
+  IMAGES_WALKING = [
+    "../img/2_character_pepe/2_walk/W-21.png",
+    "../img/2_character_pepe/2_walk/W-22.png",
+    "../img/2_character_pepe/2_walk/W-23.png",
+    "../img/2_character_pepe/2_walk/W-24.png",
+    "../img/2_character_pepe/2_walk/W-25.png",
+    "../img/2_character_pepe/2_walk/W-26.png",
+  ];
+  currentImage = 0;
+  world;
 
-
-height = 280;
-y= 155;
-speed = 10;
-IMAGES_WALKING = [
-  '../img/2_character_pepe/2_walk/W-21.png',
-  '../img/2_character_pepe/2_walk/W-22.png',
-  '../img/2_character_pepe/2_walk/W-23.png',
-  '../img/2_character_pepe/2_walk/W-24.png',
-  '../img/2_character_pepe/2_walk/W-25.png',
-  '../img/2_character_pepe/2_walk/W-26.png',
-];
-currentImage = 0;
-world;
-
-constructor() {
-    super().loadImage('../img/2_character_pepe/2_walk/W-21.png');
+  constructor() {
+    super().loadImage("../img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
     this.animate();
   }
 
-  animate(){
-
+  animate() {
     setInterval(() => {
-
-      if(this.world.keyboard.RIGHT){
-      this.x += this.speed;
+      if (this.world.keyboard.RIGHT) {
+        this.x += this.speed;
+        this.otherDirection = false;
       }
 
-      if(this.world.keyboard.LEFT){
+      if (this.world.keyboard.LEFT) {
         this.x -= this.speed;
-        }
+        this.otherDirection = true;
+      }
+    }, 1000 / 60);
 
-    }, 1000/60);
-
-
-    setInterval( () => {
-
-      if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT){
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
 
         //Walk Animation
-        let i =  this.currentImage % this.IMAGES_WALKING.length;  //let i = 0 % 6; 0, Rest 0 // i = 0, 1, 2, 3, 4, 5, 0
+        let i = this.currentImage % this.IMAGES_WALKING.length; //let i = 0 % 6; 0, Rest 0 // i = 0, 1, 2, 3, 4, 5, 0
         let path = this.IMAGES_WALKING[i];
         this.img = this.imageCache[path];
         this.currentImage++;
       }
-    }, 50)
+    }, 50);
   }
 
   jump() {}
