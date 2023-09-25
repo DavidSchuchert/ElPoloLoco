0c69fb6327e705fe6c7c328b6ea9fdec4d102045
DavidSchuchert <Davidschuchert@icloud.com>
2023-09-25 17:34:17 +0200

    added Endboss

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -23,6 +23,13 @@ class MovableObject {
     });
 
   }
+
+  playAnimation(images){
+    let i = this.currentImage % this.IMAGES_WALKING.length; //let i = 0 % 6; 0, Rest 0 // i = 0, 1, 2, 3, 4, 5, 0
+    let path = images[i];
+    this.img = this.imageCache[path];
+    this.currentImage++;
+  }
   moveRight() {
     console.log("Moving right");
   }
