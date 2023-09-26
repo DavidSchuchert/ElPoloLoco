a1d7f108cf361af04bd4dfdd64c5cdbd4585d7fe
DavidSchuchert <Davidschuchert@icloud.com>
2023-09-26 18:31:37 +0200

    added Collision Borders

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -37,6 +37,19 @@ class MovableObject {
     });
   }
 
+  draw(ctx){
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+
+  drawFrame(ctx){
+    
+    ctx.beginPath();
+    ctx.lineWidth = '5';
+    ctx.strokeStyle = 'blue';
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.stroke();
+  }
+
   playAnimation(images) {
     let i = this.currentImage % this.IMAGES_WALKING.length; //let i = 0 % 6; 0, Rest 0 // i = 0, 1, 2, 3, 4, 5, 0
     let path = images[i];
