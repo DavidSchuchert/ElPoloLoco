21836667c1040a4c26608322b720254712bdc1b8
DavidSchuchert <Davidschuchert@icloud.com>
2023-09-26 17:36:12 +0200

    added Gravity to character

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -5,7 +5,23 @@ class MovableObject {
   height = 150;
   width = 100;
   imageCache = {};
-  speed = 0.15
+  speed = 0.15;
+  speedY = 0;
+  accerleration = 1;
+
+  applyGravity() {
+    setInterval(() => {
+      if (this.isAboveGround()) {
+        this.y -= this.speedY;
+        this.speedY -= this.accerleration;
+      }
+    }, 1000 / 25);
+  }
+
+  isAboveGround() {
+    return this.y < 140;
+  }
+
   otherDirection = false;
 
   //loadImage('./img/img.png')
@@ -16,15 +32,13 @@ class MovableObject {
 
   loadImages(arr) {
     arr.forEach((path) => {
-
       let img = new Image();
       img.src = path;
       this.imageCache[path] = img;
     });
-
   }
 
-  playAnimation(images){
+  playAnimation(images) {
     let i = this.currentImage % this.IMAGES_WALKING.length; //let i = 0 % 6; 0, Rest 0 // i = 0, 1, 2, 3, 4, 5, 0
     let path = images[i];
     this.img = this.imageCache[path];
