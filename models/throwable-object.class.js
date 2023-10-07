ab4c3827ab72cdca5bb7b873c0496a53fe3ab214
DavidSchuchert <Davidschuchert@icloud.com>
2023-10-07 13:00:15 +0200

    startet with bottle animation

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -8,6 +8,14 @@ class ThrowableObject extends MovableObject {
    * @param {number} x - The initial x-coordinate of the object.
    * @param {number} y - The initial y-coordinate of the object.
    */
+
+  BOTTLE_SPIN = [
+    "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
+    "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
+    "img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
+    "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
+  ];
+  
   constructor(x, y) {
     super().loadImage("../img/6_salsa_bottle/salsa_bottle.png");
     this.x = x;
@@ -15,6 +23,15 @@ class ThrowableObject extends MovableObject {
     this.height = 60;
     this.width = 50;
     this.throw(100, 200);
+    this.loadImages(this.BOTTLE_SPIN);
+    this.animate();
+  }
+
+
+  animate() {
+    setInterval(() => {
+        this.playAnimation(this.BOTTLE_SPIN);
+    }, 100);
   }
 
   /**
