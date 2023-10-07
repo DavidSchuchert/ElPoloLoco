524c5f2aa60f285b412e50d6894de80e2ab952bc
DavidSchuchert <Davidschuchert@icloud.com>
2023-10-07 13:10:07 +0200

    startet with bottle animation

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -3,35 +3,36 @@
  * @extends MovableObject
  */
 class ThrowableObject extends MovableObject {
-  /**
+
+
+  IMAGES_BOTTLE_SPIN = [
+    "/img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
+    "/img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
+    "/img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
+    "/img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
+  ];
+  
+    /**
    * Creates a new ThrowableObject instance.
    * @param {number} x - The initial x-coordinate of the object.
    * @param {number} y - The initial y-coordinate of the object.
    */
-
-  BOTTLE_SPIN = [
-    "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
-    "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
-    "img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
-    "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
-  ];
-  
   constructor(x, y) {
-    super().loadImage("../img/6_salsa_bottle/salsa_bottle.png");
+    super().loadImage("../img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png");
+    this.loadImages(this.IMAGES_BOTTLE_SPIN);
     this.x = x;
     this.y = y;
     this.height = 60;
     this.width = 50;
     this.throw(100, 200);
-    this.loadImages(this.BOTTLE_SPIN);
     this.animate();
   }
 
 
   animate() {
     setInterval(() => {
-        this.playAnimation(this.BOTTLE_SPIN);
-    }, 100);
+      this.playAnimation(this.IMAGES_BOTTLE_SPIN);
+  }, 50);
   }
 
   /**
